351c56b4db1db87c42d009c617ee372f95ae2cb9
KathleenHwang <yell301@naver.com>
2015-03-08 00:28:53 +0900

    [추가] 기보 정보 클래스 추가

---
+++ app/src/main/java/snsproject/com/naver/cafe/stone/GiboInfo.java
@@ -0,0 +1,44 @@
+package snsproject.com.naver.cafe.stone;
+
+/**
+ * Created by ws on 2015-03-07.
+ */
+public class GiboInfo {
+    private float mTypeName;
+    private char mColor;
+    private int mX;
+    private int mY;
+    private int mCnt;
+    private int mId;
+    private int mParentId;
+
+    public GiboInfo(float typeName, char color, int x, int y, int cnt, int id, int pId) {
+        mTypeName = typeName;
+        mColor = color;
+        mX = x;
+        mY = y;
+        mCnt = cnt;
+        mId = id;
+        mParentId = pId;
+    }
+
+    public char getColor() {
+        return mColor;
+    }
+
+    public int getX() {
+        return mX;
+    }
+
+    public int getY() {
+        return mY;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public int getParentId() {
+        return mParentId;
+    }
+}
